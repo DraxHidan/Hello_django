91d601ea5941a5d068b60ceda99f0691ff53c06e
DraxHidan <rafael2002mf@gmail.com>
2023-09-25 08:34:25 -0300

    commit da primeira aplicação Django. Hello World

---
+++ core/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, HttpResponse
+
+
+# Create your views here.
+
+
+def hello(request, nome, idade):
+    return HttpResponse(f'<h1>Hello {nome} de {idade} anos<h1>')
+
